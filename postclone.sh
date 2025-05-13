3f04a20a27b668f0c8f7b1af1db2eb0e5d034bad
leah <leah.j.bertrandATgmail.com>
2025-05-12 18:07:20 -0600

    add volume, tweak layout

--- postclone.sh
+++ postclone.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #first-time-only setup steps 
+echo "copying videos"
 cp ../GOES-renders/* ./backend/GOES-renders/
 cd backend
 source ~/.bashrc
+echo "seeding server"
 python3 CloudPrinter.py --seed SEED
 cd ../frontend
+echo "installing node packages"
 npm install
